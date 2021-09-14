871d3283f87cfd9b07062cc29e9d0baa56898e7c
Snta_Claus <7103@ctucareer.co.za>
2021-09-14 21:13:35 +0200

    you were last seen here!

---
+++ Summative_7103/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Summative_7103.Startup))]
+namespace Summative_7103
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
